17f162d5553e8371d58e8362513f302379eb7293
Nikhil Giramkar <niksgiramkar@gmail.com>
2023-11-26 20:05:22 +0530

    Making <nullable> as disabled in API.csproj Exploring Program.cs, refer notes

--- Restore/API/Program.cs
+++ Restore/API/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//Here we add dependancy injections
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -10,16 +10,17 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+//Here we add middlewares
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection(); //Will not use this for dev, will use this in Production, hence commented
 
-app.UseAuthorization();
+app.UseAuthorization(); //For Auth
 
-app.MapControllers();
+app.MapControllers(); //For mapping request to correct controller
 
-app.Run();
+app.Run(); //Terminal middleware
