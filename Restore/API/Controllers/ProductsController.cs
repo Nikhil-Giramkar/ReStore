e7721e45f46159532933d577ce7dd334c3efbfaf
Nikhil Giramkar <niksgiramkar@gmail.com>
2023-11-27 22:47:45 +0530

    Async Await Task APIs

--- Restore/API/Controllers/ProductsController.cs
+++ Restore/API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -8,24 +9,22 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext context;
+        private readonly StoreContext _context;
         public ProductsController(StoreContext context)
         {
-            this.context = context;
+            _context = context;
         }
 
         [HttpGet]
-        public ActionResult<List<Product>> GetProducts(){
-            var products = context.Products.ToList();
-
-            //Ok Status: 200 response
-            return Ok(products);
+        public async Task<ActionResult<List<Product>>> GetProducts(){
+            //Fetches list of products from store.db > Products table
+            return await _context.Products.ToListAsync();
         }
 
         [HttpGet("{id}")] //api/products/3
-        public ActionResult<Product> GetProduct(int id){
+        public async Task<ActionResult<Product>> GetProduct(int id){
 
-            return context.Products.Find(id);
+            return await _context.Products.FindAsync(id);
         }
     }
 }
