7a42ef5eb2ae8b55ad801880d5fde689b987d45a
Nikhil Giramkar <niksgiramkar@gmail.com>
2023-11-29 22:39:39 +0530

    Product.ts Interface added and used

--- Restore/client/src/App.tsx
+++ Restore/client/src/App.tsx
@@ -1,38 +1,42 @@
 import React, { useEffect } from "react"
+import { Product } from "./Product";
 
 function App() {
 
   //Initializing products state, with an array of objects
-  const [products, setProducts] = React.useState(
-    [{name: 'product 1', price: 100.00},
-    {name: 'product 2', price: 200.00}]
-    )
+  const [products, setProducts] = React.useState<Product[]>([])
 
-    function addProduct(){
-      setProducts(prevState => [
-        ...prevState, //Retaining previous values via spread operator.
-        {
-          name: 'product '+(prevState.length + 1), //Increment name
-          price: (prevState.length*100 + 100) //Increment price
-        }
-      ])
-    }
+  function addProduct() {
+    setProducts(prevState => [
+      ...prevState, //Retaining previous values via spread operator.
+      {
+        id: prevState.length + 101,
+        name: 'product ' + (prevState.length + 1), //Increment name
+        price: (prevState.length * 100 + 100), //Increment price
+        pictureUrl: "http://picsum/photos/200", //Some random URL 
+        type: "some type",
+        brand: "some brand",
+        quantityInStock: Math.random() * 100,
+        description: "some desc",
+      }
+    ])
+  }
 
-    //UseEffect 
-    useEffect(()=>{
-      fetch('http://localhost:5000/api/products') 
+  //UseEffect 
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
       .then(res => res.json()) //promise returned
       .then(data => setProducts(data)); //promise returned -> state initialized
-    }
-    ,[]) //Adding an empty depenedancy array, so that we fetch data only once when app loads, else we will be trapped in endless loop
+  }
+    , []) //Adding an empty depenedancy array, so that we fetch data only once when app loads, else we will be trapped in endless loop
 
 
   return (
     <div>
       <h1>Re-Store</h1>
       <ul>
-        {products.map((item, idx) => ( //Map function to add li tag to each item
-          <li key={idx}>{item.name} - {item.price}</li> //Adding index as unique key (for stable identity)
+        {products.map((product) => ( //Map function to add li tag to each item
+          <li key={product.id}>{product.name} - {product.price}</li> //Adding index as unique key (for stable identity)
         ))}
       </ul>
 
