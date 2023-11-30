cc97794fcdb83ea8f27bcc83757c10dc7e700dc7
Nikhil Giramkar <niksgiramkar@gmail.com>
2023-11-30 22:14:07 +0530

    Styling Product Card with Material UI. Rendering product info in cards

--- Restore/client/src/features/catalog/ProductCard.tsx
+++ Restore/client/src/features/catalog/ProductCard.tsx
@@ -1,5 +1,6 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
 import { Product } from "../../app/models/Product";
+import { FontDownloadOutlined } from "@mui/icons-material";
 
 interface Props {
     product: Product
@@ -9,23 +10,33 @@ export default function ProductCard({ product }: Props) {
     return (
         <>
             <Card>
+                <CardHeader 
+                    avatar={
+                        <Avatar sx={{bgcolor: 'secondary.main'}}> 
+                            {product.name.charAt(0).toLocaleUpperCase()}
+                        </Avatar>
+                    }
+                    title={product.name}
+                    titleTypographyProps={{
+                        sx: {fontWeight: 'bold', color: 'primary.main'}
+                    }}
+                />
                 <CardMedia
-                    sx={{ height: 140 }}
-                    image="http://picsum.photos/200"
-                    title="green iguana"
+                    sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'warning.light'}} // to fit image inside the space
+                    image={product.pictureUrl}
+                    title={product.name}
                 />
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Lizard
+                    <Typography gutterBottom color='secondary' variant="h5">
+                        Rs.{(product.price / 100).toFixed(2)}
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                        species, ranging across all continents except Antarctica
+                        {product.brand} / {product.type}
                     </Typography>
                 </CardContent>
                 <CardActions>
-                    <Button size="small">Share</Button>
-                    <Button size="small">Learn More</Button>
+                    <Button size="small">Add to Cart</Button>
+                    <Button size="small">View</Button>
                 </CardActions>
             </Card>
         </>
