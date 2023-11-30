c5adfecf536274c06fa53a5037f8f0d85e7f283a
Nikhil Giramkar <niksgiramkar@gmail.com>
2023-11-30 22:16:48 +0530

    Cleaning up App.tsx

--- Restore/client/src/app/layout/App.tsx
+++ Restore/client/src/app/layout/App.tsx
@@ -1,47 +1,15 @@
-import React, { useEffect } from "react"
-import { Product } from "../models/Product";
 import { Catalog } from "../../features/catalog/Catalog";
 import Header from "./Header";
 import { Container } from "@mui/material";
 
 function App() {
-
-  //Initializing products state, with an array of objects
-  const [products, setProducts] = React.useState<Product[]>([])
-
-  function addProduct() {
-    setProducts(prevState => [
-      ...prevState, //Retaining previous values via spread operator.
-      {
-        id: prevState.length + 101,
-        name: 'product ' + (prevState.length + 1), //Increment name
-        price: (prevState.length * 100 + 100), //Increment price
-        pictureUrl: "http://picsum/photos/200", //Some random URL 
-        type: "some type",
-        brand: "some brand",
-        quantityInStock: Math.random() * 100,
-        description: "some desc",
-      }
-    ])
-  }
-
-  //UseEffect 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-      .then(res => res.json()) //promise returned
-      .then(data => setProducts(data)); //promise returned -> state initialized
-  }
-    , []) //Adding an empty depenedancy array, so that we fetch data only once when app loads, else we will be trapped in endless loop
-
-
   return (
     <>
-  
+
       <Header />
 
       <Container>
-      <Catalog products={products} 
-              addProduct={addProduct}/>
+        <Catalog />
       </Container>
 
 
--- Restore/client/src/features/catalog/Catalog.tsx
+++ Restore/client/src/features/catalog/Catalog.tsx
@@ -1,18 +1,23 @@
-import { Button} from "@mui/material";
+import React, { useEffect } from "react";
 import { Product } from "../../app/models/Product"
 import ProductList from "./ProductList";
 
-interface Props {
-    products: Product[]; //products is an array of type Product
-    addProduct: () => void; // addProduct is a method with no parameter and void return type
-}
+export function Catalog() {
 
-export function Catalog({products, addProduct}: Props) {
+    //Initializing products state, with an array of objects
+    const [products, setProducts] = React.useState<Product[]>([])
+
+    //UseEffect 
+    useEffect(() => {
+        fetch('http://localhost:5000/api/products')
+            .then(res => res.json()) //promise returned
+            .then(data => setProducts(data)); //promise returned -> state initialized
+    }
+        , []) //Adding an empty depenedancy array, so that we fetch data only once when app loads, else we will be trapped in endless loop
+
     return (
         <>
-            <ProductList products={products}/>
-            <Button variant='contained' onClick={addProduct}>Add Product</Button>
-
+            <ProductList products={products} />
         </>
     )
 }
