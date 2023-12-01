322a2c5be6b309d2a7e07db2b84a0b9356875442
Nikhil Giramkar <niksgiramkar@gmail.com>
2023-12-01 22:05:09 +0530

    Links for Contact, Catalog, About, Login, Register added

--- Restore/client/src/app/layout/Header.tsx
+++ Restore/client/src/app/layout/Header.tsx
@@ -1,8 +1,21 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { AppBar, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
 interface Props{
     darkMode: boolean;
     handleThemeChange : () => void;
 }
+
+const midLinks = [
+    {title: 'catalog', path: '/catalog'},
+    {title: 'about', path: '/about'},
+    {title: 'contact', path: '/contact'},
+]
+
+const rightLinks = [
+    {title: 'login', path: '/login'},
+    {title: 'register', path: '/register'},
+]
+
 export default function Header({darkMode, handleThemeChange} : Props){
     return (
         <>
@@ -10,6 +23,34 @@ export default function Header({darkMode, handleThemeChange} : Props){
             <Toolbar>
                 <Typography variant='h6'>RE-STORE</Typography>
                 <Switch checked={darkMode} onChange={handleThemeChange} />
+
+                <List sx={{display: 'flex'}}>
+                    {midLinks.map(({title, path}) => (
+                        <ListItem
+                            component={NavLink} // List Item rendered as NavLink
+                            to={path} //to property specifies path to navigate to
+                            key={path} //Need to add a key
+                            sx={{color: 'inherit', typography: 'h6'}}
+                            >
+                            {title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
+                
+                <List sx={{display: 'flex'}}>
+                    {rightLinks.map(({title, path}) => (
+                        <ListItem
+                            component={NavLink} // List Item rendered as NavLink
+                            to={path} //to property specifies path to navigate to
+                            key={path} //Need to add a key
+                            sx={{color: 'inherit', typography: 'h6'}}
+                            >
+                            {title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
+
+
             </Toolbar>
         </AppBar>
         </>
