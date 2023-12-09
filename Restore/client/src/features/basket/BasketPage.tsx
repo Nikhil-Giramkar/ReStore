ee21bc8df69e36735d3b8e26c3f84880381996b7
Nikhil Giramkar <niksgiramkar@gmail.com>
2023-12-09 15:44:32 +0530

    Styled BasketPage

--- Restore/client/src/features/basket/BasketPage.tsx
+++ Restore/client/src/features/basket/BasketPage.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react"
 import { Basket } from "../../app/models/Basket";
 import agent from "../../app/api/agent";
 import LoadingComponent from "../../app/layout/LoadingComponent";
-import { Typography } from "@mui/material";
+import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Delete } from "@mui/icons-material";
 
 export default function BasketPage() {
     const [loading, setLoading] = useState(true);
@@ -24,8 +25,39 @@ export default function BasketPage() {
 
     return (
         <>
-        <h1>Basket</h1>
-        <h3>BuyerId = {basket?.buyerId}</h3>
+        <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Product</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Quantity</TableCell>
+            <TableCell align="right">Subtotal</TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {basket?.items.map((item) => (
+            <TableRow
+              key={item.productId}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {item.name}
+              </TableCell>
+              <TableCell align="right">Rs.{(item.price / 100).toFixed(2)}</TableCell>
+              <TableCell align="right">{item.quantity}</TableCell>
+              <TableCell align="right">Rs.{((item.price * item.quantity)/100).toFixed(2)}</TableCell>
+              <TableCell align="right">
+                <IconButton color="error">
+                    <Delete />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
         </>
     )
 }
