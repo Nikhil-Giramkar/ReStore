50541efe0632ab3356ce775b63582b812a04cd5f
Nikhil Giramkar <niksgiramkar@gmail.com>
2023-12-13 22:19:39 +0530

    1. useDispatch( ) - to pass action type 2. useSelector( ) - for listening states changes 3. return new state from reducer based on action type. 4. Created buttons to dispatch actions

--- Restore/client/src/features/contact/ContactPage.tsx
+++ Restore/client/src/features/contact/ContactPage.tsx
@@ -1,10 +1,13 @@
-import { Typography } from "@mui/material";
-import { useSelector } from "react-redux";
-import { CounterState } from "./counterReducer";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { CounterState, DECREMENET_COUNTER, INCREMENET_COUNTER } from "./counterReducer";
 
 export default function ContactPage(){
 
-    const {data, title} = useSelector((state : CounterState) => state);
+    const dispatch = useDispatch(); //Used to dispatch actions
+    const {data, title} = useSelector((state : CounterState) => state); //useSelector listens to the state and updates them, wherever used (Like event handling in C#)
+    //Whenever, data or title is updated in store, it is listened here, React re-renders this component.
+    
     return (
         <>
         <Typography variant="h2">
@@ -14,6 +17,11 @@ export default function ContactPage(){
         <Typography variant="h5">
             The data is : {data}
         </Typography>
+
+        <ButtonGroup>
+            <Button variant="contained" color="error" onClick={() => dispatch({type: DECREMENET_COUNTER})}>Decrement</Button>
+            <Button variant="contained" color="primary" onClick={() => dispatch({type: INCREMENET_COUNTER})}>Increment</Button>
+        </ButtonGroup>
         </>
     )
 }
--- Restore/client/src/features/contact/counterReducer.ts
+++ Restore/client/src/features/contact/counterReducer.ts
@@ -1,3 +1,6 @@
+export const INCREMENET_COUNTER = "INCREMENET_COUNTER";
+export const DECREMENET_COUNTER = "DECREMENET_COUNTER";
+
 export interface CounterState {
     data: number;
     title: string;
@@ -8,6 +11,22 @@ const initialState: CounterState = {
     title: 'YARC (Yet Another Redux Counter)'
 }
 
-export default function counterReducer(state = initialState, action: any){
-    return state;
+export default function counterReducer(state = initialState, action: any) {
+    switch (action.type) {
+        case INCREMENET_COUNTER:
+            //return state.data + 1 //This is not allowed, this is called mutating the state
+            return {
+                ...state,
+                data: state.data + 1 // In this way, we replaced an existing property with a new one
+            }
+
+        case DECREMENET_COUNTER:
+            return {
+                ...state,
+                data: state.data - 1
+            }
+
+        default:
+            return state;
+    }
 }
