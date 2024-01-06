7f474cd4d750861299fc7c5a2fde13ba7230f24e
Nikhil Giramkar <niksgiramkar@gmail.com>
2024-01-06 15:24:23 +0530

    Used FormProvider from react hook to save the content of Address page even on switching pages.

--- Restore/client/src/features/checkout/AddressForm.tsx
+++ Restore/client/src/features/checkout/AddressForm.tsx
@@ -1,69 +1,65 @@
-import { Typography, Grid, FormControlLabel, Checkbox, Button } from "@mui/material";
-import { useForm } from "react-hook-form";
+import { Typography, Grid, FormControlLabel, Checkbox } from "@mui/material";
+import { useFormContext } from "react-hook-form";
 import AppTextInput from "../../app/components/AppTextInput";
 
 export default function AddressForm() {
-    const { control, handleSubmit } = useForm()
+    const { control } = useFormContext()
 
     return (
         <>
             <Typography variant="h6" gutterBottom>
                 Shipping address
             </Typography>
-            <form onSubmit={handleSubmit((data) => console.log(data))}>
-                <Grid container spacing={3}>
-                    <Grid item xs={12} sm={12}>
-                        <AppTextInput control={control}
-                            name='fullname'
-                            label='Full Name'
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <AppTextInput control={control}
-                            name='address1'
-                            label='Address 1'
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <AppTextInput control={control}
-                            name='address2'
-                            label='Address 2'
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <AppTextInput control={control}
-                            name='city'
-                            label='City'
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <AppTextInput control={control}
-                            name='state'
-                            label='State'
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <AppTextInput control={control}
-                            name='zip'
-                            label='Zipcode'
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <AppTextInput control={control}
-                            name='country'
-                            label='Country'
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <FormControlLabel
-                            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-                            label="Use this address for payment details"
-                        />
-                    </Grid>
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={12}>
+                    <AppTextInput control={control}
+                        name='fullname'
+                        label='Full Name'
+                    />
                 </Grid>
-
-                <Button type="submit"> Submit Form</Button>
-            </form>
+                <Grid item xs={12}>
+                    <AppTextInput control={control}
+                        name='address1'
+                        label='Address 1'
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <AppTextInput control={control}
+                        name='address2'
+                        label='Address 2'
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput control={control}
+                        name='city'
+                        label='City'
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput control={control}
+                        name='state'
+                        label='State'
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput control={control}
+                        name='zip'
+                        label='Zipcode'
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput control={control}
+                        name='country'
+                        label='Country'
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <FormControlLabel
+                        control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
+                        label="Use this address for payment details"
+                    />
+                </Grid>
+            </Grid>
         </>
     );
 }
