e3241cdd1ec8d66e76cf2bcb7328e3d52de4fa43
Nikhil Giramkar <niksgiramkar@gmail.com>
2024-01-06 16:47:38 +0530

    Create Order Request sent to API on last step. And successfully created in DB

--- Restore/client/src/app/api/agent.ts
+++ Restore/client/src/app/api/agent.ts
@@ -102,11 +102,17 @@ const Account = {
     currentUser: () => requests.get("account/currentUser"),
 }
 
+const Orders = {
+    list: () => requests.get('orders'),
+    fetch: (id: number) => requests.get(`orders/${id}`),
+    create: (values: any) => requests.post('orders', values)
+}
 const agent = {
     Catalog,
     TestErrors,
     Basket,
-    Account
+    Account,
+    Orders
 }
 
 export default agent;
--- Restore/client/src/features/checkout/CheckoutPage.tsx
+++ Restore/client/src/features/checkout/CheckoutPage.tsx
@@ -6,6 +6,10 @@ import Review from "./Review";
 import { FieldValues, FormProvider, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { validationSchema } from "./checkoutValidation";
+import agent from "../../app/api/agent";
+import { useAppDispactch } from "../../app/store/configureStore";
+import { clearBasket } from "../basket/basketSlice";
+import { LoadingButton } from "@mui/lab";
 
 const steps = ['Shipping address', 'Review your order', 'Payment details'];
 
@@ -25,6 +29,10 @@ function getStepContent(step: number) {
 export default function CheckoutPage() {
 
     const [activeStep, setActiveStep] = useState(0);
+    const [orderNumber, setOrderNumber] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const dispatch = useAppDispactch();
+
     const currentValidationSchema = validationSchema[activeStep];
 
     const methods = useForm({
@@ -32,10 +40,25 @@ export default function CheckoutPage() {
         resolver: yupResolver(currentValidationSchema)
     });
 
-    const handleNext = (data: FieldValues) => {
-        if (activeStep === 2)
-            console.log(data)
-        setActiveStep(activeStep + 1);
+    const handleNext = async (data: FieldValues) => {
+        const { nameOnCard, saveAddress, ...shippingAddress } = data;
+
+        if (activeStep === steps.length - 1) {
+            setLoading(true);
+            try {
+                const orderNumber = await agent.Orders.create({saveAddress, shippingAddress});
+                setOrderNumber(orderNumber);
+                setActiveStep(activeStep + 1);
+                dispatch(clearBasket());
+                setLoading(false);
+            } catch (error) {
+                console.log(error)
+                setLoading(false);
+            }
+        }
+        else{
+            setActiveStep(activeStep + 1);
+        }
     };
 
     const handleBack = () => {
@@ -63,9 +86,9 @@ export default function CheckoutPage() {
                                 Thank you for your order.
                             </Typography>
                             <Typography variant="subtitle1">
-                                Your order number is #2001539. We have emailed your order
-                                confirmation, and will send you an update when your order has
-                                shipped.
+                                Your order number is #{orderNumber}. We have not emailed your order
+                                confirmation, and will not send you an update when your order has
+                                shipped as this is a fake store!
                             </Typography>
                         </>
                     ) : (
@@ -77,14 +100,15 @@ export default function CheckoutPage() {
                                         Back
                                     </Button>
                                 )}
-                                <Button
+                                <LoadingButton
+                                    loading = {loading}
                                     disabled={!methods.formState.isValid}
                                     variant="contained"
                                     type='submit'
                                     sx={{ mt: 3, ml: 1 }}
                                 >
                                     {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
-                                </Button>
+                                </LoadingButton>
                             </Box>
                         </form>
                     )}
