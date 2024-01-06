5ab6d856c9d392de0970b35d0f58138195b5461a
Nikhil Giramkar <niksgiramkar@gmail.com>
2024-01-06 16:18:34 +0530

    Seperating the Basket Table in a different component so that it can be re-used in Basket  and Review Form of Checkout.
    Added isBasket check to display buttons only when we are viewing Basket and not Review Form of Checkout

--- Restore/client/src/features/basket/BasketPage.tsx
+++ Restore/client/src/features/basket/BasketPage.tsx
@@ -1,84 +1,19 @@
-import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, Grid, Button } from "@mui/material";
-import { Add, Delete, Remove } from "@mui/icons-material";
-import { LoadingButton } from "@mui/lab";
+import { Typography, Grid, Button } from "@mui/material";
 import BasketSummary from "./BasketSummary";
-import { currencyFormat } from "../../app/util/util";
 import { Link } from "react-router-dom";
-import { useAppDispactch, useAppSelector } from "../../app/store/configureStore";
-import { addBasketItemAsync, removeBasketItemAsync } from "./basketSlice";
+import { useAppSelector } from "../../app/store/configureStore";
+import BasketTable from "./BasketTable";
 
 export default function BasketPage() {
 
-  const {basket, status} = useAppSelector(state => state.basket);
-  const dispatch = useAppDispactch();
+  const {basket} = useAppSelector(state => state.basket);
 
   if (!basket)
     return <Typography variant="h3">Your basket is empty</Typography>
 
   return (
     <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Product</TableCell>
-              <TableCell align="right">Price</TableCell>
-              <TableCell align="center">Quantity</TableCell>
-              <TableCell align="right">Subtotal</TableCell>
-              <TableCell align="right"></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {basket?.items.map((item) => (
-              <TableRow
-                key={item.productId}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  <Box display="flex" alignItems="center">
-                    <img src={item.pictureUrl} alt={item.name} style={{ height: 50, marginRight: 20 }} />
-                    <span>{item.name}</span>
-                  </Box>
-                </TableCell>
-                <TableCell align="right">{currencyFormat(item.price)}</TableCell>
-                <TableCell align="center">
-
-                  <LoadingButton
-                    loading={status === ('pendingRemoveItem'+ item.productId + 'rem')}
-                    color="error"
-                    onClick={() => dispatch(
-                      removeBasketItemAsync(
-                        {productId: item.productId, quantity: 1, name: 'rem'}))}>
-                    <Remove />
-                  </LoadingButton>
-
-                  {item.quantity}
-
-                  <LoadingButton
-                    loading={status === ('pendingAddItem'+ item.productId)}
-                    color="secondary"
-                    onClick={() => dispatch(addBasketItemAsync({productId: item.productId}))}>
-                    <Add />
-                  </LoadingButton>
-
-                </TableCell>
-                <TableCell align="right">{currencyFormat(item.price * item.quantity)}</TableCell>
-                <TableCell align="right">
-                  <LoadingButton loading={status === ('pendingRemoveItem'+ item.productId + 'del')}
-                    color="error"
-                    onClick={() => dispatch(
-                      removeBasketItemAsync(
-                        {productId: item.productId, quantity: item.quantity, name: 'del'}))}>
-                    <Delete />
-                  </LoadingButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-
-
+      <BasketTable items={basket.items} />
       <Grid container>
         <Grid item xs={6} />
         <Grid item xs={6}>
